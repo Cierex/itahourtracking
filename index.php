45e4a231bf31d56d901e7dc07ed9de06481fd501
Sean4Doyle <36742233+Sean4Doyle@users.noreply.github.com>
2018-08-22 16:41:42 -0400

    start to do

--- index.php
+++ index.php
@@ -5,14 +5,32 @@ To change this template file, choose Tools | Templates
 and open the template in the editor.
 -->
 <html>
+    <style>
+    </style>
     <head>
         <meta charset="UTF-8">
-        <title></title>
+        <title>TITLE PORTION</title>
+        <link rel="stylesheet" href="styles.css">
     </head>
     <body>
-        hello
-        <?php
-        // put your code here
-        ?>
+        Here is the body
+        
+        <!--
+        
+        To Do:
+        -Title
+        -3x3 Table with whatever information you wanted
+        -Ordered list with 5 items
+        -Line breaks to separate 
+        
+        https://www.w3schools.com
+        
+        -->        
+        <hr>
+        line
+        <br>
+        test
+        <br>
+        <br>
     </body>
 </html>
