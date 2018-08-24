f333f5ea4b593b3b7f06be64d633496849ca948f
Sean4Doyle <36742233+Sean4Doyle@users.noreply.github.com>
2018-08-24 16:41:44 -0400

    Broken basepath stuff + no db connection

--- index.php
+++ index.php
@@ -1,17 +1,10 @@
-<!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
-<html>
-    <head>
-        <meta charset="UTF-8">
-        <title></title>
-    </head>
-    <body>
-        <?php
-        // put your code here
-        ?>
-    </body>
-</html>
+<?php include 'common/configuration.php'?>
+<?php include 'view/header.php'?>     
+<h1>Main Menu</h1>
+
+<p><a href="register/index.php">Register New Employee</a></p>
+<p><a href="record_login_form.php">Record Hours</a></p>
+<p><a href="administrative_login_form.php">Administrative</a></p>    
+<p><a href="hash_test_form.php">Hash testing</a></p>    
+
+<?php include 'view/footer.php'?>
